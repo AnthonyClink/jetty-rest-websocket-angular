b7b0296152bc3a0c88db95e3080df656fa40a364
Knut Esten Melandsø Nekså <knutm@mesan.no>
2013-10-23 17:08:46 +0200

    Initial commit, a hello world REST-service using jetty.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Wiki
+
+## Build instructions
+- Clone git repository
+- Run: mvn jetty:run (a server will start on port 8080)
+- http://localhost:8080/helloWorld is the URL to the hello world REST-service
+
---
+++ src/main/java/no/mesan/HelloWorld.java
@@ -0,0 +1,18 @@
+package no.mesan;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ * RESTful hello world-application using jetty.
+ *
+ * @author Knut Esten Melandsø Nekså
+ */
+
+@Path("/helloWorld")
+public class HelloWorld {
+    @GET
+    public String helloWorld() {
+        return "Hello world!";
+    }
+}
