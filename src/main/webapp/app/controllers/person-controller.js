c52839e9e8e859a3fbbe08976cc6a12a01cf78f0
Knut Esten Melandsø Nekså <knutm@mesan.no>
2014-01-24 16:22:22 +0100

    Reomved the old anguarjs project structure

--- src/main/webapp/app/controllers/person-controller.js
+++
@@ -1,9 +0,0 @@
-app.controller('PersonController', function($scope, $http) {
-    $http({method: 'GET', url: '/person'})
-        .success(function (persons){
-            $scope.persons = persons;
-        })
-        .error(function(data, status, headers, config) {
-            console.error("Superbad error has occured.");
-        });
-});
